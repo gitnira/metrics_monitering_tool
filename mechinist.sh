22101bf9efc29020eaa8da9461d0f1bdc28d0396
Ryohei Nirazuka <44765674+gitnira@users.noreply.github.com>
2020-11-04 23:55:26 +0900

    first commit

---
+++ mechinist.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+## CPU利用率を%単位で表示
+CPU_USED=$(uptime | awk '{print $10}' | sed -e 's/,$//g')
+CPU_USED_PER=$(echo "100 * ${CPU_USED}" | bc | sed -e "s/.00$//g")
+
+## メモリ利用率を%単位で表示
+MEM_USED=$(free -m | sed -n 2p | awk '{print $2}')
+MEM_USED_PER=$(echo "${MEM_USED} * 0.05" | bc)
+
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ${api_key}" https://gw.machinist.iij.jp/endpoint -d @- << EOS
+{
+  "agent": "Home",
+  "metrics": [
+    {
+      "name": "cpu used",
+      "namespace": "minecraft",
+      "data_point": {
+        "value": ${CPU_USED_PER}
+      }
+    },
+    {
+      "name": "mem used",
+      "namespace": "minecraft",
+      "data_point": {
+        "value": ${MEM_USED_PER}
+      }
+    }
+  ]
+}
+EOS
