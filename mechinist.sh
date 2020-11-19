a0d74d07d980ba2abbabb93274857a909966adbe
Ryohei Nirazuka <44765674+gitnira@users.noreply.github.com>
2020-11-20 01:23:47 +0900

    総メモリを8GBに変更

--- mechinist.sh
+++ mechinist.sh
@@ -6,7 +6,7 @@ CPU_USED_PER=$(echo "100 * ${CPU_USED}" | bc | sed -e "s/.00$//g")
 
 ## メモリ利用率を%単位で表示
 MEM_USED=$(free -m | sed -n 2p | awk '{print $3}')
-MEM_USED_PER=$(echo "${MEM_USED} * 0.05" | bc)
+MEM_USED_PER=$(echo "${MEM_USED} * 0.0125" | bc)
 
 
 curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ${api_key}" https://gw.machinist.iij.jp/endpoint -d @- << EOS
